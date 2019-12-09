30284e7f0e1993f2d7d341a60cfb1bcfa01315c2
Jonathan Truong <jjtruong90@gmail.com>
2019-12-09 10:24:50 -0800

    Updates liri.js

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -20,31 +20,65 @@ let bandsInTown = (keys.bandsInTown)
 var axios = require('axios');
 
 //Take User Command and Input//
-var userCommand = process.argv[2];
+var command = process.argv[2];
 var input = process.argv[3].slice(2).join(' ');
 
+//Switch between commands//
+function userCommand(command,input){
+switch(command){
+    case "concertThis":
+        concertThis();
+        break;
+    case "spotifyThisSong":
+        spotifyThisSong();
+        break;
+    case "movieThis":
+        movieThis();
+        break;
+    case "doWhatItSays":
+        doWhatItSays(input);
+        break;
+    default:
+        console.log("Please try again");
+        break;
+    }
+}
+userCommand(command,input);
 
 
 
 ///concert-this
-// axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-//   function(response) {
-//     console.log(response.data);
-//   })
+function concertThis(){
 
+axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp").then(
+  function(response) {
+    console.log(response.data);
+  })
+}
 
 
 
 ///movie-this
-  axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-    function(response) {
-      console.log("The movie's rating is: " + response.data.imdbRating);
-    })
+function movieThis(){
+
+}
+;//   axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+//     function(response) {
+//       console.log("The movie's rating is: " + response.data.imdbRating);
+//     })
   
 
 ///spotify this song
+function spotifyThisSong(){
+
+
+}
 
 
 
 
 ///do what it says
+function doWhatItSays(){
+
+    
+}
